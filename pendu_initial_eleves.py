179728639a19447d086cf0b6e42119db87220c2a
Cosmopolux <100239192+Cosmopolux@users.noreply.github.com>
2022-10-23 17:45:20 +0200

    Add files via upload

---
+++ pendu_initial_eleves.py
@@ -0,0 +1,68 @@
+def dessinPendu(index):
+        """ renvoie le dessin du pendu d'indice donné """
+        tab = ['''
+
+       +---+
+       |   |
+           |
+           |
+           |
+           |
+     =========''', '''
+ 
+       +---+
+       |   |
+       O   |
+           |
+           |
+           |
+    =========''', '''
+ 
+       +---+
+       |   |
+       O   |
+       |   |
+           |
+           |
+     =========''', '''
+    
+       +---+
+       |   |
+       O   |
+      /|   |
+           |
+           |
+     =========''', '''
+  
+       +---+
+       |   |
+       O   |
+      /|\  |
+           |
+           |
+     =========''', '''
+  
+       +---+
+       |   |
+       O   |
+      /|\  |
+      /    |
+           |
+     =========''', '''
+ 
+       +---+
+       O   |
+      /|\  |
+      / \  |
+           |
+     =========''']
+        
+        return tab[index]
+
+
+# Ouverture du fichier, liste_mots contient la liste des mots du fichier
+# Attention, les mots sont suivis du caractère retour à la ligne
+fichier = open("mots.txt",'r')
+liste_mots = fichier.readlines()
+fichier.close()
+
