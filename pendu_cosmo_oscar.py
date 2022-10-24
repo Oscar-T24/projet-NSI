fb73a918e77073a10b170d777d86d63b4ef6531b
Oscar-T24 <o.tesniere24@ejm.org>
2022-10-24 13:24:29 +0200

    MDRR J4AI TRYHARD

--- pendu_cosmo_oscar.py
+++ pendu_cosmo_oscar.py
@@ -15,7 +15,7 @@ for i in mot_mystere[0:-1]: # j'ajoute le -1 pour pas que le nombre d'underscore
         mot_trouve.append('_')
 # rajouter les traits d'unions s'ils existent(car ce ne sont pas des caracteres)...
 print(' '.join(mot_trouve))
-
+print(' '.join(mot_mystere))
 
 def jeu_pendu():
     '''
@@ -25,29 +25,40 @@ def jeu_pendu():
     demande à l'utilisateur une lettre
     '''
     caracteres_essai = [] 
-    mot_substitue = []
+    mot_substitue = len(mot_mystere)*['']
+    old_mot_subsitue = 0
     stage = 7
     while stage != 0: # condition de jeu : tant que l'utilisateur n'a pas envcore gangé ou perdu
         n = len(caracteres_essai)
+        old_mot_substitue = mot_substitue.count('')
+        #print('comptt:',old_mot_substitue)
         # ---------------- ENTREE DES CARCATERES --------------------------
         while True:
+            old_mot_substitue = mot_substitue.count('')
             caracteres_essai.append(entree_utilisateur(input("entrez un caractère     "),caracteres_essai))
             caracteres_essai = [i for i in caracteres_essai if i != None and i != '']
             print("carctères déja essayés",caracteres_essai)
             if len(caracteres_essai) > n:
                 break
         # ---------------------------------------------------------------------
-            print('', flush=True)
+        print('', flush=True)
         for i in range(len(mot_mystere)):
             print("_", end = ''),
             for e in caracteres_essai:
-                if mot_mystere[i] == e or e == mot_mystere[i]:
-                    mot_substitue.insert(i,mot_mystere[i])
+                if mot_mystere[i] == e or e == mot_mystere[i]:                   
                     print(mot_mystere[i].upper(), end = ''),
+                    mot_substitue[i] = mot_mystere[i] # cette partie pose probleme
+        #print('nouveau compte',mot_substitue.count(''))
+                
+        #si aucun mot n'a été trouvé comme bon entre temps, alors il n'y aura pas moins de ' ' dans mot_susbtitue
+        print(old_mot_substitue, ' VS' , mot_substitue.count(''))
+        if old_mot_substitue == mot_substitue.count(''):
+            stage -=1
         print('\n','mot substitué : ',' '.join(mot_substitue))
+        print('\n','mot substitué : ',mot_substitue)
         print("\033[94m {}\033[00m" .format(f"il reste {stage} essais"))
-        print(dessinPendu(7-stage))
-        stage -= 1
+        print(dessinPendu(8-stage))
+        
    #---------------------------------------a---------------------------------
     # A FAIRE  : CONDITION QUI VERIFIE SI L'UTILISATEUR A GAGNE OU PAS
 def entree_utilisateur(entree,essais):
