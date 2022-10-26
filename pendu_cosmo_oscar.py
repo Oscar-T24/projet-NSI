a2097f2f7bd09638d7fb041e5715c881b16dacca
Oscar-T24 <o.tesniere24@ejm.org>
2022-10-26 22:45:25 +0200

    added support for merged strings

--- pendu_cosmo_oscar.py
+++ pendu_cosmo_oscar.py
@@ -46,9 +46,10 @@ def jeu_pendu(niveau):
         # ---------------- ENTREE DES CARACTERES --------------------------
         while True:
             old_mot_substitue = mot_substitue.count('')
-            caracteres_essai.append(entree_utilisateur(input("entrez un caractère     "),caracteres_essai))
+            caracteres_essai.extend(list(entree_utilisateur(input("entrez un caractère     "),caracteres_essai)))
             caracteres_essai = [i for i in caracteres_essai if i != None and i != '']
-            print("carctères déja essayés",caracteres_essai)
+            
+            print("carctères déja essayés : ",caracteres_essai)
             if len(caracteres_essai) > n:
                 break
         # ---------------------------------------------------------------------
@@ -58,7 +59,7 @@ def jeu_pendu(niveau):
             for e in caracteres_essai:
                 if mot_mystere[i] == e or e == mot_mystere[i]:                   
                     print(mot_mystere[i].upper(), end = ''),
-                    mot_substitue[i] = mot_mystere[i] # cette partie pose probleme
+                    mot_substitue[i] = mot_mystere[i] 
         #print('nouveau compte',mot_substitue.count(''))
                 
         #si aucun mot n'a été trouvé comme bon entre temps, alors il n'y aura pas moins de ' ' dans mot_susbtitue
@@ -78,12 +79,18 @@ def entree_utilisateur(entree,essais):
     '''
     actualise la liste des caracteres donnés par l'utilisateur(entree) en supprimant les doublons
     renvoi un message d'erreur si l'utilisateur a déja rentré un caractere precedent(dans les essais)
-    '''
-    if entree.lower() not in essais:
-        return entree.lower()
-    else:
-        print("\033[31m {}\033[00m" .format('caractère déja essayé ! veuillez rentrer un autre caractère',Flush=False))
-        return None
+    '''  
+    txt = []
+    entree = entree.lower()
+    if len(entree) > 1:
+        txt = [item.split('-') for item in essais]
+        txt = [item for l in entree for item in l]
+        if txt not in essais:
+            return txt
+    if entree not in essais:
+        return entree
+    print("\033[31m {}\033[00m" .format('caractère déja essayé ! veuillez rentrer un autre caractère',Flush=False))
+    return None
     #-----------------------------------------------------------------
 
 # ======================= code moteur ============================================
