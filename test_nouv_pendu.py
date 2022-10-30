92a0a134bcbc99cf0864890a9c5c18da1dcc15cd
Cosmopolux <cosmo.bordier@gmail.com>
2022-10-30 19:25:28 -0400

    Update test_nouv_pendu.py
    CODE FIABLE AUX INSTRUCTIONS DE LA PROF:
    + Améliorations Niv2
      > pourcentage de victoires et celui de défaites.
    + Améliorations Niv1
      > return pour fonction jeu_pendu corrigé
      > main ajouté

--- test_nouv_pendu.py
+++ test_nouv_pendu.py
@@ -1,46 +1,59 @@
 from random import randint
 from pendu_initial_eleves import dessinPendu
+import os
+import sys
+os.system("")
 
 def jeu_pendu():
-    
     fichier = open("mots.txt",'r')
     liste_mots = fichier.readlines()
     fichier.close()
+
     mot_mystere = (liste_mots[randint(1,323470)]) # tirer au hasard un mot appelé ensuite mot_mystere dans le fichier mots.txt
     # l’ordinateur devra choisir un mot auhasard
-    mot_trouve = (len(mot_mystere)-1)*["_"] # définir mot_trouve comme une liste de caractères (et non comme une chaîne de caractères) de la longueur du mot à deviner
-    # en utilisant des tirets pour les lettres inconnues
+    
+    # mot_trouve = (len(mot_mystere)-1)*["_"] NE COMPTE PAS LES TRAITS D'UNIONS
+    
+    mot_trouve = [] # définir mot_trouve comme une liste de caractères (et non comme une chaîne de caractères) de la longueur du mot à deviner
+    for i in mot_mystere[0:-1]: # j'ajoute le -1 pour pas que le nombre d'underscore depasse le nombre de caracteres
+        if i == '-':
+            mot_trouve.append("-")
+        else:
+            mot_trouve.append('_')
+    # rajouter les traits d'unions s'ils existent(car ce ne sont pas des caracteres)... en utilisant des tirets pour les lettres inconnues
+    
     print(" *----------LE JEU DU PENDU:----------* \n")
     print(' '.join(mot_trouve))
     print(mot_mystere) # option triche #
     
     stage = 0
     L = []
-    while stage < 6 and (' '.join(mot_trouve)).rstrip() != (' '.join(mot_mystere).rstrip()):
+    while stage < 6 and (''.join(mot_trouve)).rstrip() != (''.join(mot_mystere).rstrip()): # Tant que le pendu n'est pas finit et que le mot ne sois pas trouvé...
         l = input("entrez un caractère: ") # demander au joueur une lettre
         # demander des lettres à l’utilisateur
-        miseajour_mot(mot_mystere, mot_trouve, l)
         if miseajour_mot(mot_mystere, mot_trouve, l) == True:
             print(f"La lettre '{l}' est bien dans le mot a deviner.")
             if stage != 0:
-                print(dessinPendu(0+stage)) # afficher le pendu si le mot n’a pas été trouvé après le nombre d’essais permis par les dessins
+                print(dessinPendu(stage)) # afficher le pendu si le mot n’a pas été trouvé après le nombre d’essais permis par les dessins
                 # l’état du pendu   
         else:
             stage += 1
             print(f"\nLa lettre '{l}' n'est pas dans le mot a deviner.")
-            print(dessinPendu(0+stage)) # afficher le pendu si le mot n’a pas été trouvé après le nombre d’essais permis par les dessins
+            print(dessinPendu(stage)) # afficher le pendu si le mot n’a pas été trouvé après le nombre d’essais permis par les dessins
             # l’état du pendu
         print(' '.join(mot_trouve)) # afficher l’état d’avancement de mot_trouve
-        # les lettres du mot déjà devinées
         L += l
-        print("\nCarctères déja essayés: ",' // '.join(L))
+        print("\nCarctères déja essayés: ",' // '.join(L)) # les lettres du mot déjà devinées
         # affichant après chaque proposition
         print("\n *-----------------------------------------------* \n")
     
+    jeu_pendu.var = (''.join(mot_mystere).rstrip())
+    
     if stage < 6:
-        print(f"Le mot a trouver était bien {mot_mystere} \n Victoire ! \n") # terminer le jeu si toutes les lettres du mot ont été trouvées.
+        return 'Victoire'
     else:
-        print(f"Défaite :-( \n Le mot a trouver était {mot_mystere}")
+        return 'Défaite'
+#renvoie le mot ‘victoire’ ou ‘defaite’ selon le résultat.
         
 def miseajour_mot(mot_mystere, mot_trouve, l):
     '''
@@ -58,4 +71,32 @@ def miseajour_mot(mot_mystere, mot_trouve, l):
     else: 
         return False
 
-jeu_pendu() 
+def main():
+    """
+    execute le code principal 
+    """
+    pourcentage_victoire = 0
+    pourcentage_defaite = 0
+    effectif_victoire = 0
+    effectif_defaite = 0
+    total = 0
+    re = ''         
+    while True:
+        if input(f'voulez vous {re}jouer au pendu? [o/n]') == 'o':
+            if jeu_pendu() == 'Victoire':
+                effectif_victoire += 1
+                print(f"Le mot a trouver était bien '{jeu_pendu.var}' \n Victoire ! \n") # terminer le jeu si toutes les lettres du mot ont été trouvées.
+            else:
+                print(f"Défaite, désole ca sera pour la prochaine fois ;-) \n le mot était \033[1m{jeu_pendu.var}\033[0m")
+                effectif_defaite += 1
+            total += 1
+            pourcentage_victoire = round(effectif_victoire * 100 / total)
+            pourcentage_defaite = round(effectif_defaite * 100 / total)
+            print(f"Pourcentage de 'Victoire // Défaite': {pourcentage_victoire} % // {pourcentage_defaite} % ")
+            if total > 0 : 
+                re = 're'
+        else:
+            print('au revoir !')
+            break
+
+main()
