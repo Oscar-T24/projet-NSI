c6e3b62eea9b088f24841b8b87fd55cf621716d6
Oscar-T24 <o.tesniere24@ejm.org>
2022-11-06 00:03:39 +0100

    Update READ.py

--- READ.py
+++ READ.py
@@ -6,7 +6,7 @@ import re
 
 while True:
   #TS = urllib.request.urlopen("https://api.thingspeak.com/channels/1922406/feeds.json?results=1")
-  TS = urllib.request.urlopen(" https://api.thingspeak.com/channels/1922406/fields/1.json?api_key=XEPU2C2CXF5SWCMM&results=5")
+  TS = urllib.request.urlopen(" https://api.thingspeak.com/channels/1922406/fields/1.json?api_key=XEPU2C2CXF5SWCMM&results=5") #on peux changer le nombre de variables recues avec reulsts
   response = TS.read()
   data=json.loads(response)
   pprint(data) 
