5a7fcff20ba424d170335bb27c440c8d56e0abbb
Cosmopolux <cosmo.bordier@gmail.com>
2022-11-10 15:41:08 +0100

    Update 4.0
    > Changement commentaires
    > Optimisation
    > Update nom fichier 'test'

--- test_nouv_pendu.py
+++ test_nouv_pendu.py
@@ -6,56 +6,60 @@ import os
 import time
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
-# si le code bloque sur la biblioteque urllib sur Mac OS 
-    # ==> executer le fichier sur Machintosh HD > Applications > Pyhton 3.xx > Install Certificates.comman
-    # ce fichier debloque la decouverte ssl non certifié
+# Si le code bloque sur la biblioteque urllib sur Mac OS 
+    # ==> Executer le fichier sur Machintosh HD > Applications > Pyhton 3.xx > Install Certificates.comman
+    # Ce fichier debloque la decouverte ssl non certifié
+
 def cls():
     '''
-    nettoie le 'buffer' de l'ecran pour plus de lisibilité
+    Nettoie le 'buffer' de l'ecran pour plus de lisibilité
     None
     '''
     os.system('cls' if os.name=='nt' else 'clear')
-    #print ("\n" * 100)
+
 def jeu_pendu(niveau):
+    
     global niv 
     niv = niveau
+    
+    # ==========Lecture mot.txt========== #
     fichier = open("mots.txt",'r')
     liste_mots = fichier.readlines()
     mot_mystere = ''
     while len(mot_mystere) > (niveau+4)*(niveau+2) or len(mot_mystere) < (niveau+2)*(niveau+2):
-        mot_mystere = (liste_mots[randint(1,323470)])
-    
+        mot_mystere = (liste_mots[randint(1,323470)]) # Choix un mot au hasard dans le fichier 'mot.txt'
     fichier.close()
-    # l’ordinateur devra choisir un mot auhasard
-    
-    mot_trouve = [] # définir mot_trouve comme une liste de caractères (et non comme une chaîne de caractères) de la longueur du mot à deviner
-    for i in mot_mystere[0:-1]: # j'ajoute le -1 pour pas que le nombre d'underscore depasse le nombre de caracteres
+
+    # ==========Definition de 'mot_trouve'========== #
+    mot_trouve = []
+    for i in mot_mystere[0:-1]:
         if i == '-':
-            mot_trouve.append("-")
+            mot_trouve.append("-") # rajouter les traits d'unions s'ils existent(car ce ne sont pas des caracteres)... en utilisant des tirets pour les lettres inconnues
         else:
             mot_trouve.append('_')
-    # rajouter les traits d'unions s'ils existent(car ce ne sont pas des caracteres)... en utilisant des tirets pour les lettres inconnues
     
     cls()
     
-    print("\033[1;3m \n ✱----------✱ LE JEU DU PENDU ✱----------✱ \n \033[0m", "❖ Si a tout moment vous souhaitez deviner le mot dans son integralité tapez 'guess' ❖ \n") 
-    print(' '.join(mot_trouve))
+    print("\033[1;3m \n ✱----------✱ LE JEU DU PENDU ✱----------✱ \n \033[0m", "❖ Si a tout moment vous souhaitez deviner le mot dans son integralité tapez 'guess' ❖ \n \n",' '.join(mot_trouve))
     print(mot_mystere) # OPTION TRICHE A RETIRER 
     
-    stage = 0
-    L = []
+    stage = 0 # Pour les étapes du dessin ascii du pendu
+    L = [] # Pour la liste des caractères essayés
 
+    # ==========Boucle du jeu========== #
     while stage < 6 and (''.join(mot_trouve)).rstrip() != (''.join(mot_mystere).rstrip()): # Tant que le pendu n'est pas finit et que le mot ne sois pas trouvé...
-
-        l = input("Entrez un caractère: ").lower() # demander au joueur une lettre
+        
+        # ==========Entrée de l'utilisateur========== #
+        l = input(">> Entrez un caractère: ").lower() 
         if len(l) > 1:
             if l == 'guess':
                 if input('\n Vous avez entré la commande GUESS. Veuillez entrez votre guess : ').strip() == (''.join(mot_mystere).rstrip()):
-                    break # attention 
+                    break # 'guess' pour essayer de deviner le mot complet
                 print('Mauvais mot ! Bien essayé quand meme')
             else:
-                print("\033[31m {}\033[00m" .format('vous ne pouvez pas entrer plus de 1 carcteres à la fois !'))
-            continue
+                print("\033[31m {}\033[00m" .format('!!! vous ne pouvez pas entrer plus de 1 carcteres à la fois !!!'))
+            continue 
+        # ==========Verification de l'entrée========== #
         if l in L:
             print("\033[31m {}\033[00m".format(f"caractère '{l}' déja essayé ! veuillez rentrer un autre caractère"))
             continue
@@ -63,7 +67,6 @@ def jeu_pendu(niveau):
             print("\033[31m {}\033[00m".format(f"le caractère '{l}' n'est pas dans l'alphabet ! Notez que les mots ne contiennent pas de caracteres spéciaux"))       
             continue
 
-        # verifications du caractere entré, auquel cas ou il y aurait un probleme passer la boucle active
         cls()
         print("\033[1;3m \n ✱----------✱ LE JEU DU PENDU ✱----------✱ \n \033[0m")
         if miseajour_mot(mot_mystere, mot_trouve, l) == True:
@@ -71,11 +74,12 @@ def jeu_pendu(niveau):
         else:
             stage += 1
             print(f"La lettre '{l}' n'est pas dans le mot a deviner.")
-        print(dessinPendu(stage),"\033[94m{}\033[00m" .format("\n Tu a le droit à " + str(abs(6-stage)) + " erreur" + ("s" if 6-stage > 1 else "") + " avant d'être pendu!\n")) # afficher le pendu si le mot n’a pas été trouvé après le nombre d’essais permis par les dessins
-        print(f"\033[1;3m {' '.join(mot_trouve)}\033[0m") # affiche l’état d’avancement de mot_trouve
-        L += l # Rajoute le caractère entré par l'utilisateur à la liste des caractères essayés
-        print("\nCarctères déja essayés: ║",' ║ '.join(L)) # les lettres du mot déjà devinées... affichant après chaque proposition
-    jeu_pendu.var = (''.join(mot_mystere).rstrip()) # Pour pouvoir utiliser une variable locale dans une autre fonction
+        print(dessinPendu(stage),"\033[94m{}\033[00m" .format("\n Tu a le droit à " + str(abs(6-stage)) + " erreur" + ("s" if 6-stage > 1 else "") + " avant d'être pendu!\n"),f"\033[1;3m \n{' '.join(mot_trouve)}\n \033[0m","\n>> Carctères déja essayés: ║",' ║ '.join(L))
+        
+        L += l 
+    
+    jeu_pendu.var = (''.join(mot_mystere).rstrip()) # '.var ' pour pouvoir utiliser une variable locale dans une autre fonction
+    
     if stage < 6:
         return 'Victoire' # renvoie le mot ‘victoire’
     else:
@@ -124,12 +128,7 @@ def main():
                 niv+=2
                 score = int(total*niv)
                 print(f"⭆\x1B[4m Votre score: \x1B[0m \n \n ⮑ [ {score} ]⮐ \n ")
-                état = True
-                while True:
-                    état = publier_score(input("Veuillez entrez votre nom d'utilisateur en minuscule, en caratcres alphabétiques (sera utilisé pour le classement): "),score)
-                    match état:
-                        case True:
-                            break
+                publier_score(input("Veuillez entrez votre nom d'utilisateur en minuscule, en caratcres alphabétiques (sera utilisé pour le classement): "),score)
                 lecture_serveur_TS()
                 if total > 0 : 
                     re = 're'
