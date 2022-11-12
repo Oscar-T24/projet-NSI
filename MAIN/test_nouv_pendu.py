462ed9e4b39a8b0af69e54802d5557fa67ee8c85
Cosmopolux <cosmo.bordier@gmail.com>
2022-11-12 14:38:41 +0100

    Update 5.0
    > New folder MAIN, folder a telecharger pour jouer au jeu

--- MAIN/test_nouv_pendu.py
+++ MAIN/test_nouv_pendu.py
@@ -31,7 +31,7 @@ def jeu_pendu(niveau):
     niv = niveau
 
     # ==========Lecture mot.txt========== #
-    fichier = open("mots.txt",'r')
+    fichier = open("MAIN/mots.txt",'r')
     liste_mots = fichier.readlines()
     mot_mystere = ''
     while len(mot_mystere) > (niveau+4)*(niveau+2) or len(mot_mystere) < (niveau+2)*(niveau+2):
